571406a8f9362793279ed900dcb83c67c6645153
swastikpatro <patroswastik.jsdev@gmail.com>
2022-11-20 20:11:10 +0530

    feat: added loading as default

--- tours-project/src/App.jsx
+++ tours-project/src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from 'react';
+import Loading from './Loading';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return <div className='App'></div>;
 }
 
